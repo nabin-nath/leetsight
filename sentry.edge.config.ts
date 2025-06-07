c61262f73dae3f253a7e21152dd2b12c983c3507
nabin-nath <nabinnath9@gmail.com>
2025-06-07 14:30:41 +0530

    dev check add

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,12 +5,14 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://b5917ff93a978bea1b713695037a8dce@o4509450991173632.ingest.us.sentry.io/4509453666025472",
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    dsn: "https://b5917ff93a978bea1b713695037a8dce@o4509450991173632.ingest.us.sentry.io/4509453666025472",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
